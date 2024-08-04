71d0b0fbc56007bcd8284132115bae9e789fa27c
alex-379 <alexey_379@mail.ru>
2024-08-05 08:30:28 +1000

    initial

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,13 @@
+import './HomePage.scss'
+
+export const HomePage = () => {
+  return (
+    <div id="home-page">
+        <div>HomePage</div>
+        <div>HomePage</div>
+        <div>HomePage</div>
+        <div>HomePage</div>
+        <button>press me</button>
+    </div>
+  )
+}
---
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -0,0 +1,14 @@
+import './RegisterPage.scss'
+
+export const RegisterPage = () => {
+  return (
+    <form>
+      RegisterPage <br />
+      <label>
+        Name:
+        <input type="text" />
+      </label>
+      <button>Button</button>
+    </form>
+  )
+}
